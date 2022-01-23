16f4452ae8715ada224acc6ae04653f0f57ab40e
Jakub Ostrowski <cespenar1@gmail.com>
2022-01-23 15:51:34 +0100

    Implemented support for multiple languages. Added translation to Polish.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from pathlib import Path
 
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, lazy_gettext as _l
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -18,9 +19,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap5(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -52,5 +55,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 from app import routes, models, errors
